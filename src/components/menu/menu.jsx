c38d81cb50bced69b5339a3bdd77cb1bcfe3ffda
Cayo Santana <cc.santana.js@gmail.com>
2024-01-11 13:59:45 -0300

    Criação Componente About
    Configuração da pagina Sobre e inicialização do componente Menu.

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Menu = () => {
+    return ( 
+        <>
+            <div className='container-menu'>
+                <a href="#"><img src="" alt="logo forme" /></a>
+                <nav className='menu'>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+     );
+}
+ 
+export default Menu;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import Home from './components/home/home'
-
+import About from './components/about/about'
 import { register } from 'swiper/element/bundle'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 register();
 import 'swiper/css';
@@ -13,8 +14,14 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
