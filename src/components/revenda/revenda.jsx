a35db5e333fddf9b7fbfae2b38bc2e1306e0d10a
Cayo Santana <cc.santana.js@gmail.com>
2024-01-15 16:34:48 -0300

    Update revenda.jsx
    Atualizei o nome da imagem

--- src/components/revenda/revenda.jsx
+++ src/components/revenda/revenda.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import FormRevenda from '../form/form';
 import './revenda.css'
-import back-image from './image/background_form.png'
+import fotoback from './image/background_form.png'
 
 
 const Revenda = () => {
     return ( 
         <>
             <div className='container-revenda'>
-                <img src="" alt="" />
+                <img src={fotoback} alt="" />
                 <div className='container-form'>
                     <FormRevenda/>
                 </div>
