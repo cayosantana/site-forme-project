1723d76c87fe5aa1d80271a6ee5bb176a7971370
Cayo Santana <cc.santana.js@gmail.com>
2024-01-15 15:49:33 -0300

    Criação do componente FormRevenda

--- src/components/revenda/revenda.jsx
+++ src/components/revenda/revenda.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import FormRevenda from '../form/form';
 
 const Revenda = () => {
     return ( 
         <>
-            <div>
+            <div className='container-revenda'>
                 <h1>Revenda</h1>
+                <div className='container-form'>
+                    <FormRevenda/>
+                    
+                    
+
+                </div>
             </div>
         </>
      );
