c956e7febdeca85270f7863251c718cea8c45a86
Cayo Santana <cc.santana.js@gmail.com>
2024-01-16 14:35:37 -0300

    Update form.jsx
    Melhoria do form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,70 +1,16 @@
 import React from 'react';
-import { useState } from "react";
+import { useForm } from 'react-hook-form'
 import './form.css'
 
 const FormRevenda = () => {
+    const { register } = useForm()
 
-    const [userForm, setUserForm] = useState({
-        name: "",
-        email: "",
-        password: "",
-        profession: "0",
-        privacyTerms: false,
-    });
     
-    const [errors, setErrors] = useState({
-        name: null,
-        email: null,
-        password: null,
-        profession: null,
-        privacyTerms: null,
-    });
     
-    const handleSubmit = () => {
-        let formIsValid = true;
+    const onSubmit = () => {}
+        
     
-        if (isEmpty(userForm.name)) {
-          setErrors((prev) => ({ ...prev, name: "Name is required." }));
-          formIsValid = false;
-        } else {
-          setErrors((prev) => ({ ...prev, name: null }));
-        }
-    
-        if (isEmpty(userForm.email)) {
-          setErrors((prev) => ({ ...prev, email: "Email is required." }));
-          formIsValid = false;
-        } else {
-          setErrors((prev) => ({ ...prev, email: null }));
-        }
-    
-        if (isEmpty(userForm.password)) {
-          setErrors((prev) => ({ ...prev, password: "Password is required." }));
-          formIsValid = false;
-        } else {
-          setErrors((prev) => ({ ...prev, password: null }));
-        }
-    
-        if (userForm.profession === "0") {
-          setErrors((prev) => ({ ...prev, profession: "Profession is required." }));
-          formIsValid = false;
-        } else {
-          setErrors((prev) => ({ ...prev, profession: null }));
-        }
-    
-        if (!userForm.privacyTerms) {
-          setErrors((prev) => ({
-            ...prev,
-            privacyTerms: "You must agree with the privacy terms.",
-          }));
-          formIsValid = false;
-        } else {
-          setErrors((prev) => ({ ...prev, privacyTerms: null }));
-        }
-    
-        if (!formIsValid) return;
-    
-        alert(JSON.stringify(userForm));
-    };
+        
 
     return ( 
         <>
@@ -73,89 +19,66 @@ const FormRevenda = () => {
                     <h1 className='title-form'>Titulo</h1>
                     <label>Nome</label>
                     <input
-                        className={errors?.name && "input-error"}
+                        //className={errors?.name && "input-error"}
                         type="text"
                         placeholder="Seu nome"
-                        value={userForm.name}
-                        onChange={(e) =>
-                            setUserForm((prev) => ({ ...prev, name: e.target.value }))
-                        }
                     />
-                    {errors?.name && <p className="error-message">{errors?.name}</p>}
+                    {/*errors?.name && <p className="error-message">{errors?.name}</p>*/}
                 </div>
                 <div className="form-group">
                     <label>E-mail</label>
                     <input
-                        className={errors?.email && "input-error"}
+                        //className={errors?.email && "input-error"}
                         type="email"
                         placeholder="Seu e-mail"
-                        value={userForm.email}
-                        onChange={(e) =>
-                            setUserForm((prev) => ({ ...prev, email: e.target.value }))
-                        }
                     />
-                    {errors?.email && <p className="error-message">{errors?.email}</p>}
+                    {/*errors?.email && <p className="error-message">{errors?.email}</p>*/}
                 </div>
                 <div className="form-group">
                     <label>Senha</label>
                     <input
-                        className={errors?.password && "input-error"}
+                        //className={errors?.password && "input-error"}
                         type="password"
                         placeholder="Senha"
-                        value={userForm.password}
-                        onChange={(e) =>
-                            setUserForm((prev) => ({ ...prev, password: e.target.value }))
-                        }
                     />
-                    {errors?.password && (
+                    {/*errors?.password && (
                         <p className="error-message">{errors?.password}</p>
-                    )}
+                    )*/}
                 </div>
                 <div className="form-group">
                     <label>Profissão</label>
                     <select
-                        className={errors?.profession && "input-error"}
-                        value={userForm.profession}
-                        onChange={(e) =>
-                            setUserForm((prev) => ({ ...prev, profession: e.target.value }))
-                        }
+                        //className={errors?.profession && "input-error"}
                     >
                         <option value="0">Selecione sua profissão...</option>
                         <option value="developer">Desenvolvedor</option>
                         <option value="other">Outra</option>
                     </select>
 
-                    {errors?.profession && (
+                    {/*errors?.profession && (
                         <p className="error-message">{errors?.profession}</p>
-                    )}
+                    )*/}
                 </div>
                 <div className="form-group">
                 <div className="checkbox-group">
                 <input
                     type="checkbox"
                     name="privacy-policy"
-                    checked={userForm.privacyTerms}
-                    onChange={() =>
-                    setUserForm((prev) => ({
-                        ...prev,
-                        privacyTerms: !prev.privacyTerms,
-                    }))
-                    }
                 />
                 <label>I agree with the privacy terms.</label>
                 </div>
 
-                    {errors?.privacyTerms && (
+                    {/*errors?.privacyTerms && (
                         <p className="error-message">{errors?.privacyTerms}</p>
-                    )}
+                    )*/}
                 </div>
 
                 <div className="form-group">
-                    <button onClick={handleSubmit}>Criar conta</button>
+                    <button onClick={onSubmit}>Criar conta</button>
                 </div>
             </div>
         </>
-     );
+    );
 }
  
 export default FormRevenda;
