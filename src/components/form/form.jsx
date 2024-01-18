e91188e06bd54ddb6d0a855c147a0e7701b5f061
Cayo Santana <cc.santana.js@gmail.com>
2024-01-18 17:28:55 -0300

    Alteração input form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,5 +1,7 @@
+//Nesse componenete eu precisei instalar 02 bibliotecas (react-hook-form e validator)
 import React from 'react';
 import { useForm } from 'react-hook-form'
+import validator from 'validator';
 import './form.css'
 
 const FormRevenda = () => {
@@ -12,27 +14,58 @@ const FormRevenda = () => {
         <>
             <div className="app-container">
                 <div className="form-group">
-                    <h1 className='title-form'>Titulo</h1>
+                    <h1 className='title-form'>Seja um Revendedor</h1>
                     <label>Nome</label>
                     <input
                         className={errors?.name && "input-error"}
                         type="text"
-                        placeholder="Seu nome"
+                        placeholder="Nome"
                         {...register("name", {required: true})}
                     />
                     {errors?.name?.type === 'required' && <p className="error-message">Nome é um campo obrigatório!</p>}
+                </div>
+                <div className="form-group">
+                    <label>CNPJ</label>
+                    <input
+                        className={errors?.cnpj && "input-error"}
+                        type="text"
+                        placeholder="xx.xxx.xxx/xxxx-xx"
+                        {...register("cnpj", {required: true, minLength: 18 })}
+                    />
+                    {errors?.cnpj?.type === 'required' && (
+                        <p className="error-message">CNPJ é um campo obrigatório!</p>
+                    )}
+                    {errors?.cnpj?.type === 'minLength' && (
+                        <p className="error-message">O CNPJ deve ter no minimo 18 caracteres!</p>
+                    )}
                 </div>
                 <div className="form-group">
                     <label>E-mail</label>
                     <input
                         className={errors?.email && "input-error"}
                         type="email"
-                        placeholder="Seu e-mail"
-                        {...register("email", {required: true})}
+                        placeholder="e-mail"
+                        {...register("email", {required: true, validate: (value) => validator.isEmail(value)})}
                     />
                     {errors?.email?.type === 'required' && <p className="error-message">Email é um campo obrigatório!</p>}
+                    {errors?.email?.type === 'validate' && <p className="error-message">Email não é valido!</p>}
                 </div>
                 <div className="form-group">
+                    <label>Telefone</label>
+                    <input
+                        className={errors?.tel && "input-error"}
+                        type="tel"
+                        placeholder="(xx) xxxxx-xxxx"
+                        {...register("tel", {required: true, minLength: 15 })}
+                    />
+                    {errors?.tel?.type === 'required' && (
+                        <p className="error-message">Telefone é um campo obrigatório!</p>
+                    )}
+                    {errors?.tel?.type === 'minLength' && (
+                        <p className="error-message">O telefone deve possuir DDD!</p>
+                    )}
+                </div>
+                {/*<div className="form-group">
                     <label>Senha</label>
                     <input
                         className={errors?.password && "input-error"}
@@ -46,8 +79,8 @@ const FormRevenda = () => {
                     {errors?.password?.type === 'minLength' && (
                         <p className="error-message">A senha tem que ter no minimo 8 caracteres!</p>
                     )}
-                </div>
-                <div className="form-group">
+                </div>*/}
+                {/*<div className="form-group">
                     <label>Profissão</label>
                     <select
                         {...register("profession", { validate: (value) => {
@@ -63,7 +96,7 @@ const FormRevenda = () => {
                     {errors?.profession?.type === 'validate' && (
                         <p className="error-message">O campo Profissão é obrigatório!</p>
                     )}
-                </div>
+                    </div>*/}
                 <div className="form-group">
                 <div className="checkbox-group">
                 <input
@@ -71,7 +104,7 @@ const FormRevenda = () => {
                     name="privacy-policy"
                     {...register("privacyTerms", {required: true})}
                 />
-                <label>I agree with the privacy terms.</label>
+                <label>Politicas de privacidade e termos de uso.</label>
                 </div>
 
                     {errors?.privacyTerms?.type === 'required' && (
