4f3bf8731b3da9047188857fc57772c28ffea0a4
Cayo Santana <cc.santana.js@gmail.com>
2024-02-01 17:19:53 -0300

    Criação da CSS do carrossel
    Criação da folha de estilo do componente carrossel.

--- src/components/carrossel/carrossel.jsx
+++ src/components/carrossel/carrossel.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react'
-
-
+import './carrossel.css'
 
 
 const Carrossel = ( ) => {
     return ( 
         <>
             <div>
-                <Swiper slidesPerView={3} spaceBetween={50} navigation={true} pagination={{ clickable: false }}>
-                    <SwiperSlide>
+                <Swiper slidesPerView={3} spaceBetween={50} navigation={true} pagination={{ clickable: false }} className='swiper-carrossel'>
+                    <SwiperSlide className='swiper-slide-carrossel'>
                         <h1>Teste 01</h1>
                     </SwiperSlide>
-                    <SwiperSlide>
+                    <SwiperSlide className='swiper-slide-carrossel'>
                         <h1>Teste 02</h1>
                     </SwiperSlide>
-                    <SwiperSlide>
+                    <SwiperSlide className='swiper-slide-carrossel'>
                         <h1>Teste 03</h1>
                     </SwiperSlide>
-                    <SwiperSlide>
+                    <SwiperSlide className='swiper-slide-carrossel'>
                         <h1>Teste 04</h1>
                     </SwiperSlide>
                 </Swiper>
