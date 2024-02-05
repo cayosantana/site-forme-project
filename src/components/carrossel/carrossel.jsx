c63392c623c28da468f654a62d85af7cda641ef9
Cayo Santana <cc.santana.js@gmail.com>
2024-02-05 14:29:55 -0300

    Inserindo imagens do Carrossel
    Alimentando a pasta Image do componente carrossel.

--- src/components/carrossel/carrossel.jsx
+++ src/components/carrossel/carrossel.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react'
 import './carrossel.css'
 
+import blueStorm from './image/BLUE STORM FINAL.jpg'
+import debut from './image/DEBUT FINAL.jpg'
+import deepOcean from './image/DEEP OCEAN FINAL.jpg'
+import lemonDiamond from './image/LEMON DIAMOND FINAL.jpg'
+import mantela from './image/MANTELA FINAL.jpg'
+import marblestorme from './image/MARBLESTORME FINAL.jpg'
+import neonOcean from './image/NEON OCEAN FINAL.jpg'
+import ohaio from './image/OHAIO FINAL.jpg'
+import pinkOcean from './image/PINK OCEAN FINAL.jpg'
+import pinkStorm from './image/PINK STORM FINAL.jpg'
+import purpleDiamond from './image/PURPLE DIAMOND FINAL.jpg'
+import purpleDream from './image/PURPLE DREAM.jpg'
+import purpleOcean from './image/PURPLE OCEAN FINAL.jpg'
+import redDiamond from './image/RED DIAMOND FINAL.jpg'
+import sandStorm from './image/SANDSTORM FINAL.jpg'
+import strongRed from './image/STRONG RED FINAL.jpg'
+import sunset from './image/SUNSET FINAL.jpg'
+import tesla from './image/TESLA  FINAL.jpg'
+
 
 const Carrossel = ( ) => {
     return ( 
@@ -9,6 +28,7 @@ const Carrossel = ( ) => {
             <div>
                 <Swiper slidesPerView={3} spaceBetween={50} navigation={true} pagination={{ clickable: false }} className='swiper-carrossel'>
                     <SwiperSlide className='swiper-slide-carrossel'>
+                        <img src={''} alt=''/>
                         <h1>Teste 01</h1>
                     </SwiperSlide>
                     <SwiperSlide className='swiper-slide-carrossel'>
