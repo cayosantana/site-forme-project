dbea035be335809daea5b0d7f113ae73fa6307ed
Cayo Santana <cc.santana.js@gmail.com>
2024-02-20 18:07:36 -0300

    Atualização Responsividade Menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import logo from './image/logo_forme_p&bPrancheta 3.png'
 import './menu.css'
 
 
 const Menu = () => {
+    
     return ( 
         <>
-            <div className='container-menu'>
+            <nav>
                 <a href="/"><img className='logo' src={logo} alt="logo forme" /></a>
-                <nav className='menu'>
-                    <ul>
-                        <li><Link className='link' to="/">Inicial</Link></li>
-                        <li><Link className='link' to="about">Sobre</Link></li>
-                        <li><Link className='link-destaque' to="revenda">Seja Nosso Revendedor</Link></li>
-                    </ul>
-                </nav>
-            </div>
+                <div className='menu'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul id='navbar'>
+                    <li><NavLink id='link' className='active' to="/">Inicial</NavLink></li>
+                    <li><NavLink id='link' className='active' to="about">Sobre</NavLink></li>
+                    <li><NavLink id='link-destaque' className='active' to="revenda">Seja Nosso Revendedor</NavLink></li>
+                </ul>
+            </nav>
         </>
      );
 }
+
  
 export default Menu;
