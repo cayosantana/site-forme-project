5ba024eccc1f95baed6fb7c7ac232dde0b69c9e4
Cayo Santana <cc.santana.js@gmail.com>
2024-02-20 20:41:52 -0300

    Atualização Responsividade Menu
    em andamento

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from './image/logo_forme_p&bPrancheta 3.png'
 import './menu.css'
 
 
 const Menu = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     
     return ( 
         <>
             <nav>
                 <a href="/"><img className='logo' src={logo} alt="logo forme" /></a>
-                <div className='menu'>
+                <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
-                <ul id='navbar'>
+                <ul id='navbar' className={menuOpen ? "open" : ""}>
                     <li><NavLink id='link' className='active' to="/">Inicial</NavLink></li>
                     <li><NavLink id='link' className='active' to="about">Sobre</NavLink></li>
                     <li><NavLink id='link-destaque' className='active' to="revenda">Seja Nosso Revendedor</NavLink></li>
