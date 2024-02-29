f6f91e5b9ba31f7643f313c302fdfae57ee11bd3
Cayo Santana <cc.santana.js@gmail.com>
2024-02-29 13:52:19 -0300

    Model
    Configuração do estilo para o componente Model.

--- src/components/model/model.jsx
+++ src/components/model/model.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './model.css'
+import Footer from '../footer/footer';
 
 import modelImage from './image/Foto-modelos-forme.png'
 
@@ -7,9 +8,17 @@ const Model = () => {
     return ( 
         <>
             <div id='container-model'>
-                <h1>Teste</h1>
-
+                <img src={modelImage} alt="Foto do modelo de bermuda" />
+                <div className='container-text-model'>
+                    <h2>PHYSIQUE SHORTS</h2>
+                    <p>A FORME é pioneira nas inovações dos shorts Masculino Physique, como o ajuste interno para a cintura e os dois modelos diferentes para caber nas diferentes federações. FORME conquista mercado de Físico Masculino e Muscular em diversos países.</p>
+                    <div>
+                        <h4>São mais de 18 designs</h4>
+                        <h4>Em 02 modelos diferentes</h4>
+                    </div>
+                </div>
             </div>
+            <Footer/>
         </>
      );
 }
